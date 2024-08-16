3fb9663df0b10fe843a85adb6bc43fa5ee6d44d2
surya <surya11981.sa@gmail.com>
2024-08-16 10:41:56 +0530

    whitelisted local host

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(
     credentials: true,
     origin: [
       "http://localhost:5173",
+      "http://localhost:5174",
       "http://localhost:4173",
       "https://dev-patient.tech42.in",
     ],
